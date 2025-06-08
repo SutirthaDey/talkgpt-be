044fbc9155104ebea619cffe80da7a0f11205164
Sutirtha Dey <sutirth.dey@prediqt.it>
2025-06-08 23:59:32 +0530

    healthcheck added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthType } from './auth/enums/auth-types.enum';
+import { Auth } from './auth/decorators/auth-type.decorator';
 
 @Controller('')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Auth(AuthType.None)
   @Get('health')
   getHealth(): string {
     return 'OK';
