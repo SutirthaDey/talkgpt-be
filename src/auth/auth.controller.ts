69b3b08937a16100bb1450f42af475b900872489
Sutirtha Dey <sutirth.dey@prediqt.it>
2025-06-09 19:32:33 +0530

    Throttling added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,17 +11,20 @@ import { Auth } from './decorators/auth-type.decorator';
 import { AuthType } from './enums/auth-types.enum';
 import { GoogleTokenDto } from './dtos/google-token.dto';
 import { GenerateAccessTokenDto } from './dtos/generate-access-token';
+import { Throttle } from '@nestjs/throttler';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Throttle({ default: { limit: 5, ttl: 60000, blockDuration: 60000 } })
   @Auth(AuthType.None)
   @Post('sign-in')
   async signIn(@Body() signInDto: SignInDto) {
     return await this.authService.signIn(signInDto);
   }
 
+  @Throttle({ default: { limit: 5, ttl: 60000, blockDuration: 60000 } })
   @UseInterceptors(ClassSerializerInterceptor)
   @Auth(AuthType.None)
   @Post('sign-up')
@@ -29,6 +32,7 @@ export class AuthController {
     return await this.authService.signUp(signUpDto);
   }
 
+  @Throttle({ default: { limit: 5, ttl: 60000, blockDuration: 60000 } })
   @Auth(AuthType.None)
   @Post('google-authentication')
   async googleAuthenticate(@Body() googleTokenDto: GoogleTokenDto) {
